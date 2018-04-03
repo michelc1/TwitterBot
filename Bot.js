070af57feeabad4f0df142cc04f4f5c3e4fd9400
michelc1 <chrispp10@gmail.com>
2018-04-03 00:09:17 -0400

    added config file

--- Bot.js
+++ Bot.js
@@ -1,15 +1,10 @@
 console.log("The bot is now running")
 
 var Twit = require('twit'); //import the twit module
+var config = require('./config'); //get the api keys in our config file
 
-var T = new Twit({
-  consumer_key:         'NTqP9WukLwvXoR4JxHQHuY44a',
-  consumer_secret:      'xFKAbIdKCYCv4LIVg9wzbNHnhwHJYHOtLomGQSOJr5yq9E3drY',
-  access_token:         '981010968095395840-HvWwDRT4k0UwKQryX0RJP6LFLbgRqVb',
-  access_token_secret:  'gPhHk8ltX235WepQbJ7oSLG2zBfgg4ma2LPMbdxonjSJ8',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
+var T = new Twit(config) // twitter object with our config(api keys)
 
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+T.post('statuses/update', { status: 'test!' }, function(err, data, response) {
   console.log(data)
 });
---
+++ config.js
@@ -0,0 +1,10 @@
+
+//Twitter api keys, this is how we are talking to twitter
+
+module.exports = {
+  consumer_key:         'NTqP9WukLwvXoR4JxHQHuY44a',
+  consumer_secret:      'xFKAbIdKCYCv4LIVg9wzbNHnhwHJYHOtLomGQSOJr5yq9E3drY',
+  access_token:         '981010968095395840-HvWwDRT4k0UwKQryX0RJP6LFLbgRqVb',
+  access_token_secret:  'gPhHk8ltX235WepQbJ7oSLG2zBfgg4ma2LPMbdxonjSJ8',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
